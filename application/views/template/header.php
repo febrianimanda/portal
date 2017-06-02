5c46bec7a6618839f58885420e01384f0b602028
febrianimanda <febrian.imanda@gmail.com>
2017-06-02 23:32:20 +0700

    add page template and styles

---
+++ application/views/template/header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Portal | <?= $title ?></title>
+		<link rel="stylesheet" href="<?=css_url('bootstrap.min.css');?>">
+		<link rel="stylesheet" href="<?=css_url('styles.css');?>">
+	</head>
+	<body>
