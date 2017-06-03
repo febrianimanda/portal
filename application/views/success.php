61f68924a7738a335e24aa1eee5bf6ac7f26d800
febrianimanda <febrian.imanda@gmail.com>
2017-06-03 23:45:39 +0700

    add successfull page and delete welcome page

---
+++ application/views/success.php
@@ -0,0 +1,6 @@
+<div class="col-md-12 col-centered text-center" style="padding-top: 10%">
+	<i class="glyphicon glyphicon-ok-circle" style="font-size: 48pt;"></i>
+	<h1>Selamat!</h1>
+	<h2><?= isset($job) ? $job : "undefined" ?> Telah Berhasil</h2>
+	<a href="<?= base_url() ?>">Kembali ke halaman utama</a>
+</div>
