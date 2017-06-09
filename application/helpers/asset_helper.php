7a3c5ff604e925f4d62eec6a9a00db9085746a70
febrianimanda <febrian.imanda@gmail.com>
2017-06-10 02:28:37 +0700

    add new configuration for global css and js, update asset helper

--- application/helpers/asset_helper.php
+++ application/helpers/asset_helper.php
@@ -24,11 +24,51 @@
       }
     }
 
+    //output: images folder path
     if(! function_exists('img_url'))
     {
       function img_url($uri = '')
       {
         $CI =& get_instance();
         return base_url()."asset/images/".$uri;
+      }
+    }
+
+    // output css tags
+    if(!function_exists('put_css_headers')){
+      function put_global_css(){
+        $str = '';
+        $ci =& get_instance();
+        $file_css  = $ci->config->item('global_file_css');
+        if(sizeof($file_css) > 0){
+          foreach($file_css AS $file) {
+            $str .= '<link rel="stylesheet" href="'.css_url($file).'" type="text/css" />'."\n";
+          }
+        }
+        return $str;
+      }
+    }
+
+    // output: javascript tags
+    if(!function_exists('put_js_headers')){
+      function put_global_js(){
+        $str = '';
+        $ci =& get_instance();
+        $file_js = $ci->config->item('global_file_js');
+        $url_js = $ci->config->item('global_url_js');
+
+        if(sizeof($file_js) > 0){
+          foreach($file_js AS $file) {
+            $str .= '<script type="text/javascript" src="'.js_url($file).'"></script>'."\n";
+          }
+        }
+        
+        if(sizeof($url_js) > 0){
+          foreach ($url_js as $url) {
+            $str .= '<script type="text/javascript" src="'.$url.'"></script>'."\n"; 
+          }
+        }
+
+        return $str;
       }
     }
