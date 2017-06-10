09da6355abe9c6b88c38eea71f5ae6e1c79b1a04
febrianimanda <febrian.imanda@gmail.com>
2017-06-10 12:33:47 +0700

    modifying helper

--- application/helpers/asset_helper.php
+++ application/helpers/asset_helper.php
@@ -48,6 +48,20 @@ if(!function_exists('header_css_file')){
   }
 }
 
+// output: local css tags load using URL
+if(!function_exists('css_url')){
+  function header_css_url($urls=[]){
+    $str = '';
+    $ci =& get_instance();
+    if(sizeof($urls) > 0){
+      foreach($urls AS $url) {
+        $str .= '<link rel="stylesheet" href="'.$url.'" type="text/css" />'."\n";
+      }
+    }
+    return $str;
+  }
+}
+
 // output: local javascript tags load using files
 if(!function_exists('header_js_file')){
   function header_js_file($files=[]){
