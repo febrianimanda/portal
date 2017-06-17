d7da75ae74530d3f7a5dccbbee4ea2ab4ee786dc
febrianimanda <febrian.imanda@gmail.com>
2017-06-17 23:49:50 +0700

    update registration page

--- application/views/auth/signup.php
+++ application/views/auth/signup.php
@@ -13,29 +13,24 @@
 							</div>
 							<div class="col-md-12">
 								<div class="form-group">
-									<label>Jalur Masuk</label>
-									<select name="jalur_masuk" required class="form-control">
+									<label>Jalur Masuk <span class="text-danger">*</span></label>
+									<select name="jalur" required class="form-control">
 										<option disabled selected value>Pilih Jalur Masuk</option>
-										<option value="nextgen">Next Generation (70%)</option>
-										<option value="young">Young Professional (3%)</option>
-										<option value="influencer">Influencer (3%)</option>
-										<option value="military">Military (3%)</option>
-										<option value="campus">Campus Leader (3%)</option>
-										<option value="talent">Talent Scout (10%)</option>
-										<option value="civil">Civil Organization (3%)</option>
-										<option value="servant">Public Servant (3%)</option>
+										<?php foreach ($jalur as $row): ?>
+											<option value="<?= $row->key ?>"><?= $row->details ?> (<?= $row->percentage ?>%)</option>
+										<?php endforeach; ?>
 									</select>
 								</div>
 							</div>
 							<div class="col-md-6">
 								<div class="form-group">
-									<label for="">Nama Lengkap</label>
+									<label for="">Nama Lengkap <span class="text-danger">*</span></label>
 									<input type="text" required name="fullname" placeholder="Nama Lengkap Anda" class="form-control">
 								</div>
 							</div>
 							<div class="col-md-6">
 								<div class="form-group">
-									<label for="">Email</label>
+									<label for="">Email <span class="text-danger">*</span></label>
 									<input type="email" required name="email" placeholder="Email Anda" class="form-control">
 								</div>
 							</div>
@@ -43,13 +38,13 @@
 						<div class="row">
 							<div class="col-md-6">
 								<div class="form-group">
-									<label for="">Password</label>
+									<label for="">Password <span class="text-danger">*</span></label>
 									<input name="password" required type="password" class="form-control" placeholder="Password Anda">
 								</div>
 							</div>
 							<div class="col-md-6">
 								<div class="form-group">
-									<label for="">Konfirmasi Password</label>
+									<label for="">Konfirmasi Password <span class="text-danger">*</span></label>
 									<input name="passconf" required type="password" class="form-control" placeholder="Konfirmasi Password Anda">
 								</div>
 							</div>
@@ -57,35 +52,38 @@
 						<div class="row">
 							<div class="col-md-6">
 								<div class="form-group">
-									<label for="">No. Handphone</label>
+									<label for="">No. Handphone <span class="text-danger">*</span></label>
 									<input name="handphone" required type="number" class="form-control" placeholder="No. Handphone Anda yang aktif">
 								</div>
 							</div>
 							<div class="col-md-6">
 								<div class="form-group">
-									<label for="">No. Darurat Yang Dapat Dihubungi</label>
-									<input name="handphone" required type="number" class="form-control" placeholder="Bisa Nomor teman terdekat Anda atau Orang Tua Anda">
+									<label for="">No. Darurat Yang Dapat Dihubungi <span class="text-danger">*</span></label>
+									<input name="emergency_number" required type="number" class="form-control" placeholder="Bisa Nomor teman terdekat Anda atau Orang Tua Anda">
 								</div>
 							</div>
 						</div>
 						<div class="row">
 							<div class="col-md-6">
 								<div class="form-group">
-									<label>Provinsi</label>
+									<label>Provinsi <span class="text-danger">*</span></label>
 									<select name="provinsi" class="form-control">
 										<option disabled selected value>Pilih Provinsi</option>
+										<?php foreach ($provinsi as $row): ?>
+											<option value="<?= $row->key ?>"><?= $row->nama_provinsi ?></option>
+										<?php endforeach ?>
 									</select>
 								</div>
 							</div>
 							<div class="col-md-6">
 								<div class="form-group">
-									<label>Kota / Kabupaten</label>
+									<label>Kota / Kabupaten <span class="text-danger">*</span></label>
 									<input type="text" name="kota" required class="form-control" placeholder="Kota/Kabupaten Tempat Anda Tinggal Saat Ini">
 								</div>
 							</div>
 							<div class="col-md-6">
 								<div class="form-group">
-									<label>Alamat Tinggal Saat Ini</label>
+									<label>Alamat Tinggal Saat Ini <span class="text-danger">*</span></label>
 									<textarea name="alamat" required class="form-control" rows="3" placeholder="Alamat Lengkap Tempat Tinggal Anda Saat Ini"></textarea>
 								</div>
 							</div>
@@ -96,13 +94,13 @@
 							</div>
 							<div class="col-md-6">
 								<div class="form-group">
-									<label>Nama Panggilan</label>
+									<label>Nama Panggilan <span class="text-danger">*</span></label>
 									<input name="nickname" required type="text" class="form-control" placeholder="Nama Akrab Anda">
 								</div>
 							</div>
 							<div class="col-md-6">
 								<div class="form-group">
-									<label>Tanggal Lahir</label>
+									<label>Tanggal Lahir <span class="text-danger">*</span></label>
 									<input name="birthdate" required type="date" class="form-control" placeholder="Tanggal Lahir Anda">
 								</div>
 							</div>
@@ -110,7 +108,7 @@
 						<div class="row">
 							<div class="col-md-6">
 								<div class="form-group">
-									<label>Jenis Kelamin</label>
+									<label>Jenis Kelamin <span class="text-danger">*</span></label>
 									<div class="radio">
 										<label><input type="radio" name="gender" value="laki-laki">Laki-Laki</label> &nbsp;
 										<label><input type="radio" name="gender" value="perempuan">Perempuan</label>
@@ -119,7 +117,7 @@
 							</div>
 							<div class="col-md-6">
 								<div class="form-group">
-									<label>Golongan Darah</label>
+									<label>Golongan Darah <span class="text-danger">*</span></label>
 									<div class="radio">
 										<label><input type="radio" name="golongan_darah" value="A">A</label>&nbsp;
 										<label><input type="radio" name="golongan_darah" value="B">B</label>&nbsp;
@@ -132,35 +130,32 @@
 						<div class="row">
 							<div class="col-md-6">
 								<div class="form-group">
-									<label>Agama</label>
+									<label>Agama <span class="text-danger">*</span></label>
 									<select name="agama" id="" class="form-control">
 										<option disabled selected value>Pilih Agama</option>
-										<option value="islam">Islam</option>
-										<option value="islam">Kristen Khatolik</option>
-										<option value="islam">Kristen Protestan</option>
-										<option value="islam">Hindu</option>
-										<option value="islam">Budha</option>
-										<option value="islam">Lainnya</option>
+										<?php foreach ($agama as $row): ?>
+											<option value="<?= $row->agama_id ?>"><?= $row->agama ?></option>
+										<?php endforeach; ?>
 									</select>
 								</div>
 							</div>
 							<div class="col-md-6">
 								<div class="form-group">
 									<label>Alergi Makanan</label>
-									<input type="text" name="alergi" required class="form-control" placeholder="contoh: Udang">
+									<input type="text" name="alergi_makanan" class="form-control" placeholder='contoh: Udang'>
 								</div>
 							</div>
 						</div>
 						<div class="row">
 							<div class="col-md-6">
 								<div class="form-group">
-									<label>Nama Institusi Pendidikan Terakhir</label>
+									<label>Nama Institusi Pendidikan Terakhir <span class="text-danger">*</span></label>
 									<input type="text" name="institusi" required placeholder="contoh: Telkom University" class="form-control">
 								</div>
 							</div>
 							<div class="col-md-6">
 								<div class="form-group">
-									<label>Angkatan Masuk</label>
+									<label>Angkatan Masuk <span class="text-danger">*</span></label>
 									<input type="number" name="angkatan" required class="form-control" placeholder="contoh: 2013">
 								</div>
 							</div>
@@ -168,13 +163,13 @@
 						<div class="row">
 							<div class="col-md-6">
 								<div class="form-group">
-									<label>Hobi</label>
+									<label>Hobi <span class="text-danger">*</span></label>
 									<input type="text" name="hobi" required placeholder="Pisahkan dengan koma" class="form-control" data-role="tagsinput">
 								</div>
 							</div>
 							<div class="col-md-6">
 								<div class="form-group">
-									<label>Keahlian yang dapat ditampilkan</label>
+									<label>Keahlian yang dapat ditampilkan <span class="text-danger">*</span></label>
 									<input type="text" name="pertunjukan" required class="form-control" placeholder="contoh: Tari Piring">
 								</div>
 							</div>
@@ -185,32 +180,32 @@
 							</div>
 							<div class="col-md-6">
 								<div class="form-group">
-									<label>Id Line</label>
+									<label>Id Line <span class="text-danger">*</span></label>
 									<input type="text" name="line" required placeholder="Id Line Anda yang Aktif" class="form-control">
 								</div>
 							</div>
 							<div class="col-md-6">
 								<div class="form-group">
-									<label>Link Facebook</label>
-									<input type="text" name="pertunjukan" required class="form-control" placeholder='Contoh: https://facebook.com/ForumIndonesiaMuda/'>
+									<label>Link Facebook <span class="text-danger">*</span></label>
+									<input type="text" name="fb" required class="form-control" placeholder='Contoh: https://facebook.com/ForumIndonesiaMuda/'>
 								</div>
 							</div>
 							<div class="col-md-6">
 								<div class="form-group">
-									<label>Id atau Link Twitter</label>
-									<input type="text" name="pertunjukan" required class="form-control" placeholder='Contoh: fimnews atau https://twitter.com/fimnews'>
+									<label>Id atau Link Twitter <span class="text-danger">*</span></label>
+									<input type="text" name="twitter" required class="form-control" placeholder='Contoh: fimnews atau https://twitter.com/fimnews'>
 								</div>
 							</div>
 							<div class="col-md-6">
 								<div class="form-group">
-									<label>Id atau Link Instagram</label>
-									<input type="text" name="pertunjukan" required class="form-control" placeholder='Contoh: fimnews atau https://instagram.com/fimnews'>
+									<label>Id atau Link Instagram <span class="text-danger">*</span></label>
+									<input type="text" name="instagram" required class="form-control" placeholder='Contoh: fimnews atau https://instagram.com/fimnews'>
 								</div>
 							</div>
 							<div class="col-md-6">
 								<div class="form-group">
-									<label>Link Blog atau Tumblr</label>
-									<input type="text" name="pertunjukan" required class="form-control" placeholder='Blogger, Wordpress, Tumblr atau Personal Website untuk blogging'>
+									<label>Link Blog atau Tumblr <span class="text-danger">*</span></label>
+									<input type="text" name="blog" required class="form-control" placeholder='Blogger, Wordpress, Tumblr atau Personal Website untuk blogging'>
 								</div>
 							</div>
 						</div>
