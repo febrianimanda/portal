dac9a645b2db942a60e7a20157fed0339ad668df
febrianimanda <febrian.imanda@gmail.com>
2017-07-03 22:24:27 +0700

    add coming soon page

---
+++ application/views/coming-soon.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Coming Soon | Portal FIM</title>
+	<!-- Bootstrap Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<link rel="stylesheet" href="<?= css_url('font-awesome.min') ?>">
+	<link rel="stylesheet" href="<?= css_url('comingsoon-styles') ?>" />
+</head>
+<body>
+	<div class="fluid-container">
+		<div class="row">
+			<div class="col-md-6 leftside maxheight">
+				<div class="leftcontent">
+					<h2>Stay tuned <br> We're launching soon.</h2>
+					<h1 style="color: #ff0000;">Forum Indonesia Muda 19</h1>
+					<h4>Keep in touch with us on&nbsp;
+						<a href="https://facebook.com/ForumIndonesiaMuda/"><i class="fa fa-facebook-square" alt="facebook" aria-hidden="true"></i></a>&nbsp;&nbsp;<a href="https://twitter.com/fimnews"><i class="fa fa-twitter-square" aria-hidden="true"></i></a>&nbsp;&nbsp;<a href="https://instagram.com/fimnews"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+					</h4>
+				</div>
+			</div>
+			<div class="col-md-6 rightside maxheight">
+				<div class="rightcontent text-center">
+					<img src="<?= img_url('fim.png') ?>" alt="Forum Indonesia Muda">
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- Jquery Latest Compiled and Minified JS -->
+	<script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+	<!-- Bootstrap Latest Compiled and Minified JS -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</body>
+</html>
