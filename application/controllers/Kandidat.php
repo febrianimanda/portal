52aff342d41a345f2244f9100102cb3e002bc579
febrianimanda <febrian.imanda@gmail.com>
2017-07-19 16:04:57 +0700

    penempatan statistik bugfix

--- application/controllers/Kandidat.php
+++ application/controllers/Kandidat.php
@@ -689,17 +689,19 @@ class Kandidat extends CI_Controller {
 			if($this->input->post('pilihan') == 'pusat'){
 				$this->load->model('pusat_model');
 				foreach ($arr_penempatan as $info) {
-					$this->pusat_model->update_jumlah('detail', $info);
+					echo $info."</br>";
+					$this->pusat_model->update_jumlah($info);
 				}
 			} else {
 				$this->load->model('regional_model');
 				foreach ($arr_penempatan as $info) {
-					$this->regional_model->update_jumlah('area', $info);
+					echo $info."</br>";
+					$this->regional_model->update_jumlah($info);
 				}
 			}
 		}
 		$this->alert_messages($success);
-		redirect(site_url('kandidat/pengaturan/komitmen'));
+		// redirect(site_url('kandidat/pengaturan/komitmen'));
 	}
 
 	public function do_update_akun(){
