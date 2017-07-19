680d45a6bcc67edfd1572d8a7caa1bfaeb097de5
febrianimanda <febrian.imanda@gmail.com>
2017-07-19 16:09:29 +0700

    add online chat

--- application/views/template/full-template.php
+++ application/views/template/full-template.php
@@ -18,6 +18,21 @@
 	<?php endif; ?>
 	<!-- Our CSS Style -->
 	<link rel="stylesheet" href="<?= css_url('styles') ?>" />
+	<!-- Chatra {literal} -->
+	<script>
+	    (function(d, w, c) {
+	        w.ChatraID = 'R5mt5hsqS5ypzgyxD';
+	        var s = d.createElement('script');
+	        w[c] = w[c] || function() {
+	            (w[c].q = w[c].q || []).push(arguments);
+	        };
+	        s.async = true;
+	        s.src = (d.location.protocol === 'https:' ? 'https:': 'http:')
+	        + '//call.chatra.io/chatra.js';
+	        if (d.head) d.head.appendChild(s);
+	    })(document, window, 'Chatra');
+	</script>
+	<!-- /Chatra {/literal} -->
 </head>
 <body>
 	<div class="fluid-container">
