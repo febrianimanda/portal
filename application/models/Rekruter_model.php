050006b5fa440edf8cd300f607add21c070d000f
febrianimanda <febrian.imanda@gmail.com>
2017-08-19 01:55:52 +0700

    add datatable query for rekruter list

--- application/models/Rekruter_model.php
+++ application/models/Rekruter_model.php
@@ -40,23 +40,40 @@ class Rekruter_model extends CI_Model {
 		return ($this->db->affected_rows() != 1) ? $this->db->error() : True;
 	}
 
-	public $datatable_column = array('peserta.peserta_id as peserta_id', 'peserta.profpic_path as profpic_path', 'user.jalur as jalur', 'peserta.fullname as fullname', 'peserta.institusi as institusi', 'peserta.biodata_singkat as biodata_singkat', 'peserta.video_profile as video_profile', 'perekomendasi.file_rekomendasi_path as file_rekomendasi_path', 'penilaian.nilai_cv as nilai_cv', 'penilaian.nilai_esai as nilai_esai', 'penilaian.nilai_project as nilai_project', 'penilaian.nilai_kelengkapan as nilai_kelengkapan', 'penilaian.nilai_total as nilai_total');
-	
-	public $datatable_column_order = array(null, 'peserta_id', 'profpic_path', 'jalur', 'fullname', 'institusi', 'biodata_singkat', 'video_profile', ' ile_rekomendasi_path', 'nilai_cv', 'nilai_esai', 'nilai_project', 'nilai_kelengkapan', 'nilai_total');
+	public $rekruter_table = 'rekruter';
+	public $rekruter_column = array('nama_rekruter', 'email', 'avg_cv', 'avg_esai', 'avg_berkas', 'avg_total', 'jumlah_menilai', 'is_koor');
+	public $rekruter_column_order = array(null, 'rekruter_id', 'nama_rekruter', 'email', 'avg_cv', 'avg_esai', 'avg_berkas', 'avg_total', 'jumlah_menilai', 'is_koor');
+	public $rekruter_column_search = array('email', 'nama_rekruter');
+	public $rekruter_order = array('nama_rekruter' => 'asc');
 
-	public $datatable_column_search = array('jalur', 'fullname', 'institusi');
-	public $datatable_order = array('fullname' => 'asc');
+	public $peserta_table = 'peserta';
+	public $peserta_column = array('peserta.peserta_id as peserta_id', 'peserta.profpic_path as profpic_path', 'user.jalur as jalur', 'peserta.fullname as fullname', 'peserta.institusi as institusi', 'peserta.biodata_singkat as biodata_singkat', 'peserta.video_profile as video_profile', 'perekomendasi.file_rekomendasi_path as file_rekomendasi_path', 'penilaian.nilai_cv as nilai_cv', 'penilaian.nilai_esai as nilai_esai', 'penilaian.nilai_project as nilai_project', 'penilaian.nilai_kelengkapan as nilai_kelengkapan', 'penilaian.nilai_total as nilai_total', 'rekruter.nama_rekruter');
 
-	public function get_datatable_peserta_query() {
-		$this->db->select($this->datatable_column);
-		$this->db->from('peserta');
-		$this->db->join('user', 'peserta.email = user.email', 'inner');
-		$this->db->join('penilaian', 'peserta.peserta_id = penilaian.peserta_id', 'left');
-		$this->db->join('perekomendasi', 'peserta.peserta_id = perekomendasi.peserta_id', 'left');
-		$this->db->where(array('peserta.is_completed' => 1, 'peserta.is_deleted' => 0));
+	public $peserta_column_order = array(null, 'peserta_id', 'profpic_path', 'jalur', 'fullname', 'institusi', 'biodata_singkat', 'video_profile', ' file_rekomendasi_path', 'nilai_cv', 'nilai_esai', 'nilai_project', 'nilai_kelengkapan', 'nilai_total', 'nama_rekruter');
+
+	public $peserta_column_search = array('jalur', 'fullname', 'institusi');
+	public $peserta_order = array('fullname' => 'asc');
+
+	public function get_datatable_query($table='rekruter') {
+		if($table == 'peserta') {
+			$this->db->select($this->peserta_column);
+			$this->db->from($this->peserta_table);
+			$this->db->join('user', 'peserta.email = user.email', 'inner');
+			$this->db->join('penilaian', 'peserta.peserta_id = penilaian.peserta_id', 'left');
+			$this->db->join('perekomendasi', 'peserta.peserta_id = perekomendasi.peserta_id', 'left');
+			$this->db->join('rekruter', 'penilaian.rekruter_id = rekruter.rekruter_id', 'left');
+			$this->db->where(array('peserta.is_completed' => 1, 'peserta.is_deleted' => 0));
+		} else {
+			$this->db->select($this->rekruter_column);
+			$this->db->from($this->rekruter_table);
+			$this->db->where('is_deleted', 0);
+		}
+
+		$datatable_column_search = ($table=='peserta') ? $this->peserta_column_search : $this->rekruter_column_search;
+
 		$i = 0;
 
-		foreach ($this->datatable_column_search as $item) {
+		foreach ($datatable_column_search as $item) {
 			if($_POST['search']['value']) { //search condition
 				if($i === 0) {
 					$this->db->group_start(); // open bracket clause
@@ -65,23 +82,27 @@ class Rekruter_model extends CI_Model {
 					$this->db->or_like($item, $_POST['search']['value']);
 				}
 
-				if(count($this->datatable_column_search) - 1 == $i) {
+				if(count($datatable_column_search) - 1 == $i) {
 					$this->db->group_end(); //close bracket clause
 				}
 			}
 			$i++;
 		}
 
+		$datatable_column_order = ($table=='peserta') ? $this->peserta_column_order : $this->rekruter_column_order;
+
+		$datatable_order = ($table=='peserta') ? $this->peserta_order : $this->rekruter_order;
+
 		if(isset($_POST['order'])) {
-			$this->db->order_by($this->datatable_column_order[$_POST['order'][0]['column']], $_POST['order'][0]['dir']);
-		} else if(isset($this->order)) {
-			$order = $this->order;
+			$this->db->order_by($datatable_column_order[$_POST['order'][0]['column']], $_POST['order'][0]['dir']);
+		} else if(isset($datatable_order)) {
+			$order = $datatable_order;
 			$this->db->order_by(key($order), $order[key($order)]);
 		}
 	}
 
-	public function get_datatable_peserta() {
-		$this->get_datatable_peserta_query();
+	public function get_datatable($table = 'rekruter') {
+		$this->get_datatable_query($table);
 		if($_POST['length'] != -1) {
 			$this->db->limit($_POST['length'], $_POST['start']);
 			$query = $this->db->get();
@@ -89,17 +110,22 @@ class Rekruter_model extends CI_Model {
 		}
 	}
 
-	public function count_filtered() {
-		$this->get_datatable_peserta_query();
+	public function count_filtered($table = 'rekruter') {
+		$this->get_datatable_query($table);
 		$query = $this->db->get();
 		return $query->num_rows();
 	}
 
-	public function count_all() {
-		$this->db->from('peserta');
-		$this->db->join('penilaian', 'peserta.peserta_id = penilaian.peserta_id', 'inner');
-		$this->db->join('perekomendasi', 'peserta.peserta_id = perekomendasi.peserta_id', 'inner');
-		return $this->db->count_all_results();
+	public function count_all($table = 'rekruter') {
+		if($table == 'peserta') {
+			$this->db->from('peserta');
+			$this->db->join('penilaian', 'peserta.peserta_id = penilaian.peserta_id', 'inner');
+			$this->db->join('perekomendasi', 'peserta.peserta_id = perekomendasi.peserta_id', 'inner');
+			return $this->db->count_all_results();
+		} else {
+			$this->db->from('rekruter');
+			return $this->db->count_all_results();
+		}
 	}
 
 }
